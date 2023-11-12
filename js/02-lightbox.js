1dd4aeebb92dfa44ac39150808ad7b26221adf4d
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-12 20:50:32 +0200

    edited

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -21,4 +21,7 @@ function onClick(event) {
   event.preventDefault();
 }
 
-const lightbox = new SimpleLightbox(".gallery a", {captionsData: 'alt', captionDelay: 250})
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
