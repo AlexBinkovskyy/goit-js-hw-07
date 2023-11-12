abb14ee0a62225d65fe54ae4aa2f726debe5376f
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-12 15:37:28 +0200

    first step done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,10 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const key = [];
+galleryItems.map((item) => {
+  key.push(`<li class="gallery__item"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></li>`);
+});
+galleryList.insertAdjacentHTML("afterbegin", key.join(""));
