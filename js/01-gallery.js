5b0e302cc24ae4c945333c72bac5d9c53b1ae49b
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-12 16:29:48 +0200

    add layout

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,19 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryList = document.querySelector(".gallery");
+galleryList.addEventListener("click", onClick);
 
 const key = [];
 galleryItems.map((item) => {
-  key.push(`<li class="gallery__item"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></li>`);
+  key.push(
+    `<li class="gallery__item"><img class="gallery__image" data-big-img="${item.original}" src="${item.preview}" alt="${item.description}"></li>`
+  );
 });
 galleryList.insertAdjacentHTML("afterbegin", key.join(""));
+
+function onClick(event) {
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.bigImg}" width="800" height="600">
+`);
+  instance.show();
+}
