b1c9dc393991e6c6f8b7cf8ff9163441feffed2b
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-12 20:48:31 +0200

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,18 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryList = document.querySelector(".gallery");
-// galleryList.addEventListener("click", onClick);
+galleryList.addEventListener("click", onClick);
 
 const key = [];
-galleryItems.map(({preview, original, description}) => {
+galleryItems.map(({ preview, original, description }) => {
   key.push(
     `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
-       <img class="gallery__image" src="${preview}" alt="${description}" />
+       <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
     </a>
  </li>`
   );
 });
 
 galleryList.insertAdjacentHTML("afterbegin", key.join(""));
+
+function onClick(event) {
+  event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {captionsData: 'alt', captionDelay: 250})
